838e3b46e7206daa8ab871a67f832409d7049eee
JUANSOLTEROBARRERA <72166031+JUANSOLTEROBARRERA@users.noreply.github.com>
2021-01-25 15:21:41 -0700

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/tap_u5_ejercicio2_menuestructurascontrol/MainActivity3.kt
@@ -0,0 +1,70 @@
+package mx.tecnm.tepic.tap_u5_ejercicio2_menuestructurascontrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Spinner
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main3.*
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+
+        button6.setOnClickListener {
+            var opcionSpinner = spinner.selectedItemPosition;
+
+            when(opcionSpinner){
+                0->{
+                    var cadena1 = "919";
+                    var valorEntero = cadena1.toInt()
+
+                    valorEntero +=1;
+
+                    editTextResultado.setText("Conversion CAD a INT: ${valorEntero}")
+                }
+                1->{
+                    var cadena2 = "5.18"
+                    var valorDoble = cadena2.toDouble()
+
+                    valorDoble += 0.92
+
+                    editTextResultado.setText("Conversion CAD a DOUBLE ${valorDoble}")
+                }
+                2->{
+                    var decimal = 29.8
+                    var cadena3 = decimal.toString()
+
+                    cadena3 = "Conversion DEC a CAD"+cadena3
+                    editTextResultado.setText(cadena3)
+                }
+                3->{
+                    var entero = 915
+                    var cadena4 = "Conversion INT a CAD"+entero
+
+                    editTextResultado.setText(cadena4)
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menuconversion, menu);
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.regresar->{
+
+            }
+            R.id.limpiar->{
+                editTextResultado.setText("")
+                Toast.makeText(this,"SE LIMPIO CAMPO DE TEXTO", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
+}
